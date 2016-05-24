aefb9268b2e6a4f80ab6d86c28485f9d88979de1
Kleyson Prado <kleyson@kleyson.com.br>
2016-05-23 22:11:58 -0300

    Adicionando mais bibliotecas

--- bootstrap/middlewares.js
+++ bootstrap/middlewares.js
@@ -1,4 +1,21 @@
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import cors from "cors";
+import compression from "compression";
+import helmet from "helmet";
+
 module.exports = app => {
   app.set("json spaces", 4);
   app.set("port", 3000);
+  app.use(helmet());
+  app.use(cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
+  app.use(compression());
+  app.use(bodyParser.json());
+  app.use((req, res, next) => {
+    delete req.body.id;
+    next();
+  });
 };
